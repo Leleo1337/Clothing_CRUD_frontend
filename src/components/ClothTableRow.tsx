b708c1413adde5843c5c76eab941d67e1c1867e1
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-11 18:54:22 -0300

    feat : modify some styles & added link to edit path

--- src/components/ClothTableRow.tsx
+++ src/components/ClothTableRow.tsx
@@ -1,19 +1,24 @@
+import { Link } from "react-router";
 import type { clothProps } from "../types/clothProps";
 
-export default function ClothTableRow({ id, name, price, quantity, size }: clothProps) {
+export default function ClothTableRow({ id, name, price, quantity, size, deleteCloth }: clothProps) {
    return (
       <>
-         <tr className="hover:bg-gray-100 border-b border-gray-300 transition-colors ease-in duration-100">
+         <tr className="border-b border-gray-300 transition-colors ease-in duration-100 bg-gray-200 nth-[even]:bg-gray-300">
             <td className="p-2">{id}</td>
             <td className="p-2">{name}</td>
-            <td className="p-2">{price}</td>
             <td className="p-2">{quantity}</td>
+            <td className="p-2">{price}</td>
             <td className="p-2">{size}</td>
             <td className="flex gap-2 p-2">
-               <button className="bg-amber-500 px-2 py-1 rounded-md text-white font-semibold text-sm cursor-pointer">
+               <Link
+                  to={`/Edit/${id}`}
+                  className="bg-amber-500 px-2 py-1 rounded-md text-white font-semibold text-sm cursor-pointer hover:bg-amber-600 transition ease-in duration-100">
                   Editar
-               </button>
-               <button className="bg-red-500 px-2 py-1 rounded-md text-white font-semibold text-sm cursor-pointer">
+               </Link>
+               <button
+                  onClick={deleteCloth}
+                  className="bg-red-500 px-2 py-1 rounded-md text-white font-semibold text-sm cursor-pointer hover:bg-red-600 transition ease-in duration-100">
                   Excluir
                </button>
             </td>
