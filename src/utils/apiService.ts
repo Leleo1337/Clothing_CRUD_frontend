5e2d8f9a5d0c10ae05443413d08d85f66d3a0178
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-12 18:23:01 -0300

    feat: toast will now display the response error

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -7,13 +7,13 @@ export async function getAllCloths() {
    try {
       const response = await axios.get(`${API_URL}/api/v1/cloths`);
       return response.data.data;
-   } catch (error) {
-      console.error(`something went wrong - ${error}`);
-      toast.error("Erro ao buscar roupas");
+   } catch (error: any) {
+      console.error(error);
+      toast.error(error.message);
    }
 }
 
-export async function getCloth(clothID: string) {
+export async function getCloth(clothID: string | undefined) {
    try {
       const response = await axios.get(`${API_URL}/api/v1/cloths/${clothID}`);
       return response.data.data;
@@ -27,19 +27,19 @@ export async function createCloth(formData: any) {
    try {
       const response = await axios.post(`${API_URL}/api/v1/cloths`, formData);
       return response.data;
-   } catch (error) {
-      console.error(`something went wrong - ${error}`);
-      toast.error("Erro ao criar roupa");
+   } catch (error: any) {
+      console.log('Something went wrong', error);
+      toast.error(error.response.data.msg);
    }
 }
 
-export async function updateCloth(clothID: string, formData: any) {
+export async function updateCloth(clothID: string | undefined, formData: any) {
    try {
       const response = await axios.patch(`${API_URL}/api/v1/cloths/${clothID}`, formData);
       return response.data;
-   } catch (error) {
+   } catch (error: any) {
       console.error(`something went wrong - ${error}`);
-      toast.error("Erro ao atualizar dados");
+      toast.error(error.response.data.msg);
    }
 }
 
@@ -48,8 +48,8 @@ export async function deleteCloth(clothID: string | undefined) {
    try {
       const response = await axios.delete(`${API_URL}/api/v1/cloths/${clothID}`);
       return response.data;
-   } catch (error) {
+   } catch (error: any) {
       console.error(`something went wrong - ${error}`);
-      toast.error("Erro ao deletar roupa");
+      toast.error(error.response.data.msg);
    }
 }
