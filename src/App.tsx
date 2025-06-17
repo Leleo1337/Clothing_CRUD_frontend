bea9a797606eda5a953c3ec1f036f0ab38b94f5f
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-17 17:48:37 -0300

    feat: add login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
 import Edit from "./pages/Edit";
+import Login from './pages/Login.tsx'
 
 function App() {
    return (
@@ -10,6 +11,7 @@ function App() {
          <BrowserRouter basename="/">
             <Routes>
                <Route path="/" element={<Home />} />
+               <Route path="/login" element={<Login />} />
                <Route path="/Create" element={<Create />} />
                <Route path="/Edit/:id" element={<Edit />} />
             </Routes>
