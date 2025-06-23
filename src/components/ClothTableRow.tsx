6cf136711627a30a66e28b56f675dff2f039e4f9
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-23 14:32:06 -0300

    feat: add new formattedPrice to price td

--- src/components/ClothTableRow.tsx
+++ src/components/ClothTableRow.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router";
 import type { clothProps } from "../types/clothProps";
 
-export default function ClothTableRow({ id, clothID, name, price, quantity, size, deleteCloth }: clothProps) {
+export default function ClothTableRow({ id, clothID, name, formattedPrice, quantity, size, deleteCloth }: clothProps) {
    return (
       <>
          <tr className="border-b border-gray-300 transition-colors ease-in duration-100 bg-white nth-[even]:bg-gray-700/10">
             <td className="px-4 py-2.5">{clothID}</td>
             <td className="px-4 py-2.5">{name}</td>
             <td className="px-4 py-2.5">{quantity}</td>
-            <td className="px-4 py-2.5">{price}</td>
+            <td className="px-4 py-2.5">{formattedPrice}</td>
             <td className="px-4 py-2.5">{size}</td>
             <td className="flex gap-2 p-2">
                <Link
