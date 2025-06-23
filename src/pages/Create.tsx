7fcd73648092f5978c09a4d5ce24bdf866fd8500
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-23 14:40:01 -0300

    fix: remove unused useEffect
    it was causing deploy error

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router";
 import { ArrowLeft, Box, DollarSign, Hash, RefreshCw, Ruler, Save } from "lucide-react";
 import { createCloth } from "../services/clothService";
